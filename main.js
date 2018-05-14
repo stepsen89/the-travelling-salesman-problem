b7e12d65e9f033560fec72bbd526e1510a241a12
[stepsen89] <stephanie.senoner@gmail.com>
2018-05-15 00:10:49 +0100

    shorter than

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcHNlbjg5IiwiYSI6ImNqaDZxb3Z1bDAwNGsycW11dnAwZ3N1ZXQifQ.LOs9yadH4E1wF_NcE_3Awg';
 
 var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-    center: [-74.50, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-74.50, 40],
+    zoom: 9
 });
 
 let points = [];
@@ -22,39 +22,38 @@ map.on('click', function (e) {
         
         if (points.length < 2){
           points.push(marker);
-          console.log("added");
         } else {
           console.log("not added");
-          points.forEach((e) => console.log(e._lngLat.lat));
         }
 });
 
-console.log(points);
-
 let distances = [];
-let shortest = [];
+let shorter;
 let destinationsORder = [];
+let nextDestination;
 let home;
 
 const distanceMeasurement = (points) => {
   points.forEach((e) => {
     console.log('the element is' + e._lngLat.lat);
     var from = turf.point([e._lngLat.lat, e._lngLat.lng]);
+
     points.forEach((e) => {
       var to = turf.point([e._lngLat.lat, e._lngLat.lng]);
       var options = {units: 'kilometres'};
       var distance = turf.distance(from, to, options);
-      console.log(distance);
-      distances.push(distance);
-      console.log(distances);
+      if (shorter === 0){
+        shorter = distance;
+        nextDestination = e;
+        return;
+      } else if (shorter > distance){
+        shorter = distance;
+        nextDestination = e;
+        return;
+      } else if (shorter < distance){
+        return;
+      }
     })
   })
-  return distances;
+  return distances, shorter, nextDestination;
 }
-
-console.log(distanceMeasurement(points));
-
-
-Punkt A -> B, C, D, E, F, G, H
-
-A Home
