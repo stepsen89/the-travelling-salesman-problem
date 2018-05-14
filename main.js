a455c54248baaed50d9e52209c547b5001b83203
[stepsen89] <stephanie.senoner@gmail.com>
2018-05-15 00:02:03 +0100

    adding map and marker on click, mapbox

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+'use strict'
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcHNlbjg5IiwiYSI6ImNqaDZxb3Z1bDAwNGsycW11dnAwZ3N1ZXQifQ.LOs9yadH4E1wF_NcE_3Awg';
 
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
 
+let points = [];
 
-  
+map.on('click', function (e) {
+  document.getElementById('info').innerHTML =
+      JSON.stringify(e.point) + '<br />' +
+      JSON.stringify(e.lngLat);
+
+      var marker = new mapboxgl.Marker()
+        .setLngLat([e.lngLat.lng, e.lngLat.lat])
+        .addTo(map);
+        
+        if (points.length < 2){
+          points.push(marker);
+          console.log("added");
+        } else {
+          console.log("not added");
+          points.forEach((e) => console.log(e._lngLat.lat));
+        }
+});
